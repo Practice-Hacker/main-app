5bd0b83a64c9c6875cf3f03c2305ddadd37bd0ad
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-10 18:39:08 -0600

    Add test for the welcome page.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe 'Welcome page' do
     visit root_path
 
     title = 'Practice Hacker'
+    explanation = 'This is a classical music app'
     expect(page).to have_content(title)
     expect(page).to have_link('Login With Google')
+    within '.welcome-main_content' do
+      expect(page).to have_content(explanation)
+    end
   end
 end

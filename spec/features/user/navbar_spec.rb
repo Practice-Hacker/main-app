4a94b47bc2a366912c8ea6014abf81ed64dd33b3
twarbelow <taija.warbelow@google.com>
2020-09-12 20:18:53 -0700

    navbar spec is created but incomplete, missing oauth functionality to test

---
+++ spec/features/user/navbar_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'user navbar' do
+  it 'appears on all pages after a user is logged in' do
+    visit '/'
+    # this test is incomplete, waiting on oauth functionality
+  end
+end

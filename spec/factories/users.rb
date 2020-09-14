45899cba1184daddf4a5e70085cc109e64d5c43a
Cam C <ckc.cameron@gmail.com>
2020-09-14 10:26:44 -0600

    Set up users factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    uid { "MyString" }
-    access_token { "MyString" }
-    email { "MyString" }
+    # provider { :google }
+    username { Faker::Name.first_name.downcase }
+    uid { Faker::Number.within(range: 100000..999999) }
+    access_token { Faker::Alphanumeric.alphanumeric(number: 20) }
+    sequence :email do |n|
+      "test#{:username}@test.com"
+    end
   end
 end

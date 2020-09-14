38840e76f103404d8cf9a5d3486e4e0b2369055f
twarbelow <taija.warbelow@google.com>
2020-09-14 08:55:51 -0700

    correct references

--- db/migrate/20200914155117_create_tips.rb
+++ db/migrate/20200914155117_create_tips.rb
@@ -3,8 +3,8 @@ class CreateTips < ActiveRecord::Migration[5.2]
     create_table :tips do |t|
       t.integer :difficulty_rating
       t.text :tip
-      t.references :piece_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :piece, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end

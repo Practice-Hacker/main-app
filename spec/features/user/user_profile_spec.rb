3a3e5d24dc339dc8d6e9cf9893a64ecdc7b3a6ea
Cam C <ckc.cameron@gmail.com>
2020-09-14 11:02:35 -0600

    Xit tests to pinpoint User id find error

--- spec/features/user/user_profile_spec.rb
+++ spec/features/user/user_profile_spec.rb
@@ -1,29 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "authenticated user profile page" do
-  # before :each do
-  #
-  # end
+  before :each do
+    @user = create(:user)
+  end
+
   it "shows username with their selected roles" do
-    user = User.create(username: "musiclover")
+    visit "/profile/#{@user.id}"
+    expect(current_path).to eq("/profile/#{@user.id}")
+    expect(page).to have_content(@user.username)
+    expect(page).to have_css(".user-roles")
+    # check to see if this css is not empty if user has selected roles once roles functionality added
+  end
+
+  xit "shows sections for About Me, My Skills and Pieces Contributed To" do
+    visit "/profile/#{@user.id}"
     expect(current_path).to eq("/profile")
-    expect(page).to have_content("")
+    expect(page).to have_css(".about-me")
+    expect(page).to have_css(".my-skills")
+    expect(page).to have_css(".pieces-contributed-to")
   end
 
-  it "shows sections for About Me, My Skills and Pieces Contributed To" do
-    
+  xit "allows user to edit username" do
+    visit "/profile/#{@user.id}"
+    expect(current_path).to eq("/profile")
+    expect(page).to have_content(@user.username)
+
+    click_on "Edit Profile"
+
+    fill_in :username, with: "BoomBoomClap123"
+    click_on "Submit Changes"
+
+    expect(page).to have_content("BoomBoomClap123")
+    expect(page).to_not have_content()
   end
 
-  it "allows user to edit username" do
+  xit "does not allow user to edit username to one that is already in use by another user" do
+    user2 = User.create(username: "StringsNThings")
+
+    visit "/profile/#{@user.id}"
+    expect(current_path).to eq("/profile")
+
 
   end
-
-  it "does not allow user to edit username to one that is already in use by another user" do
-
-  end
-
-  it "allows user to edit About Me and My Skills sections" do
-
-  end
-
 end

50c97a82758479c9e99c29970c79952ce4c08e6b
Cam C <ckc.cameron@gmail.com>
2020-09-14 16:23:11 -0600

    Edit uid factory for user to be number with less digits

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     # provider { :google }
     id { Faker::Number.within(range: 1..50) }
     username { Faker::Name.first_name.downcase }
-    uid { Faker::Number.within(range: 100000..999999) }
+    uid { Faker::Number.within(range: 1..100) }
     access_token { Faker::Alphanumeric.alphanumeric(number: 20) }
     sequence :email do |n|
       "test#{username}@test.com"

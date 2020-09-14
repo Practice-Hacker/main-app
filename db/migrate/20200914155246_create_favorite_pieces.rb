ebf07cbca7a85269854c441911376f793b24b5fd
twarbelow <taija.warbelow@google.com>
2020-09-14 08:56:05 -0700

    correct references

--- db/migrate/20200914155246_create_favorite_pieces.rb
+++ db/migrate/20200914155246_create_favorite_pieces.rb
@@ -1,8 +1,8 @@
 class CreateFavoritePieces < ActiveRecord::Migration[5.2]
   def change
     create_table :favorite_pieces do |t|
-      t.references :user_id, foreign_key: true
-      t.references :piece_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :piece, foreign_key: true
 
       t.timestamps
     end

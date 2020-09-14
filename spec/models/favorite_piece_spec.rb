d4fa1a701681821b23da6bfa3192d6492560bd23
twarbelow <taija.warbelow@google.com>
2020-09-14 09:16:06 -0700

    spec for favorite piece

--- spec/models/favorite_piece_spec.rb
+++ spec/models/favorite_piece_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe FavoritePiece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :piece_id }
+  end
+
+  describe 'relationships' do
+    it {should belong_to :user}
+    it {should belong_to :piece}
+  end
 end

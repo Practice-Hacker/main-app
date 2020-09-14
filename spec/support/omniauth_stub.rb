a0cf342217bca51d888ef7bc1c37750ed22d49a6
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-13 23:05:12 -0600

    Format quotes.

--- spec/support/omniauth_stub.rb
+++ spec/support/omniauth_stub.rb
@@ -4,10 +4,10 @@ OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
   uid: '12345678',
   info: {
     username: 'Tony Stark',
-    email: 'tony@stark.com',
+    email: 'tony@stark.com'
   },
   credentials: {
-    access_token: "token",
-    refresh_token: "refresh_token"
+    access_token: 'token',
+    refresh_token: 'refresh_token'
   }
 })

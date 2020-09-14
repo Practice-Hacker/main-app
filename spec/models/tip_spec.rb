d8676eddb1281451ca159ef3ce40d9950e24a7c9
twarbelow <taija.warbelow@google.com>
2020-09-14 09:16:29 -0700

    spec for tip

--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :difficulty_rating }
+    it { should validate_presence_of :tip }
+    it { should validate_presence_of :piece_id }
+    it { should validate_presence_of :user_id }
+  end
+
+  describe 'relationships' do
+    it {should belong_to :piece}
+    it {should belong_to :user}
+  end
 end

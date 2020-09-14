db1eb23dc78416fd6b0fc74383a7ec625a9b3a0b
Cam C <ckc.cameron@gmail.com>
2020-09-14 11:01:24 -0600

    Fix setup of users factories to include id

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,12 @@ require 'faker'
 FactoryBot.define do
   factory :user do
     # provider { :google }
+    id { Faker::Number.within(range: 1..50) }
     username { Faker::Name.first_name.downcase }
     uid { Faker::Number.within(range: 100000..999999) }
     access_token { Faker::Alphanumeric.alphanumeric(number: 20) }
     sequence :email do |n|
-      "test#{:username}@test.com"
+      "test#{username}@test.com"
     end
   end
 end

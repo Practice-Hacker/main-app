e4cba3fa3f773d3bb8920cea1dd8c0f6463946d0
twarbelow <taija.warbelow@google.com>
2020-09-14 09:16:21 -0700

    spec for piece

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :composer }
+  end
+  
+  describe 'relationships' do
+    it {should have_many :favorite_pieces}
+    it {should have_many :tips}
+  end
 end

e249312ac942fcdfc83abfe4902537d4164c613f
twarbelow <taija.warbelow@google.com>
2020-09-14 09:31:24 -0700

    remove validations

--- spec/models/favorite_piece_spec.rb
+++ spec/models/favorite_piece_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FavoritePiece, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of :user_id }
-    it { should validate_presence_of :piece_id }
-  end
-
   describe 'relationships' do
     it {should belong_to :user}
     it {should belong_to :piece}

a5e3171d2664d8328c60eae3f0eb4368956741bf
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-14 00:05:02 -0600

    Change the buttons to links.

--- spec/features/guest/navbar_spec.rb
+++ spec/features/guest/navbar_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'guest navbar' do
   it 'has search, browse, register, and log in' do
     visit root_path
-    expect(page).to have_button("Browse Music")
+    expect(page).to have_link("Browse Music")
     expect(page).to have_field("Search for Music")
-    expect(page).to have_button("Log In")
-    expect(page).to have_button("Register")
-    expect(page).to_not have_button("Home")
-    expect(page).to_not have_button("Profile")
-    expect(page).to_not have_button("Favorites")
+    expect(page).to have_link("Log In")
+    expect(page).to have_link("Register")
+    expect(page).to_not have_link("Home")
+    expect(page).to_not have_link("Profile")
+    expect(page).to_not have_link("Favorites")
   end
 end

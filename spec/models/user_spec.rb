5533409af8f3dd8d0123b9d864a30e9ec306ca66
twarbelow <taija.warbelow@google.com>
2020-09-14 09:18:43 -0700

    spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "relationships" do
+  describe 'validations' do
+    it { should validate_presence_of :username }
+    it { should validate_presence_of :uid }
+    it { should validate_presence_of :access_token }
+  end
+
+  describe 'relationships' do
+    it {should have_many :favorite_pieces}
+    it {should have_many :tips}
   end
 end

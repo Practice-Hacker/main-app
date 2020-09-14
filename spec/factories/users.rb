d685549a403c948ed33134bf8d7d631b949e522d
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-14 00:05:43 -0600

    Update factories.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    uid { "MyString" }
-    access_token { "MyString" }
-    email { "MyString" }
+    username { 'MyString' }
+    uid { 'MyString' }
+    access_token { 'MyString' }
+    email { 'MyString' }
+    role { 'student' }
+    about_me { 'yay!!!' }
   end
 end

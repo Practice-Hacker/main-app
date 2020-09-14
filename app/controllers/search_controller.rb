a4e4d6035ddb7dd1666688a54f5ac9d5b7f7e79c
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-14 10:18:20 -0600

    Update the controller with the facade.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,5 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(ENV['API_SINATRA_URL'])
-    data = conn.get('search') do |f|
-      f.params[:q] = params[:q]
-    end
-
-    parsed_data = JSON.parse(data.body, symbolize_names: true)
-    @pieces = parsed_data[:results].map do |result|
-      Piece.new(result)
-    end
-    binding.pry
+    @pieces = SearchFacade.pieces(params[:q])
   end
 end

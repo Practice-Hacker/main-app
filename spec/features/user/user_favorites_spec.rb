7ec3ec3f232718b7a30ffb85e297c08443feda18
Eric Hale <eric_hale@comcast.net>
2020-09-14 18:25:44 -0600

    Add more dummy data
    Co-authored-by: Taija<10294841+twarbelow@users.noreply.github.com>
    Co-authored-by: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>
    Co-authored-by: Cam Chery <57038617+ckccameron@users.noreply.github.com>

--- spec/features/user/user_favorites_spec.rb
+++ spec/features/user/user_favorites_spec.rb
@@ -6,8 +6,26 @@ RSpec.describe "As an authenticated user" do
     piece1 = FactoryBot.create(:piece)
     piece2 = FactoryBot.create(:piece)
     piece3 = FactoryBot.create(:piece)
+    piece4 = FactoryBot.create(:piece)
+    piece5 = FactoryBot.create(:piece)
+    piece6 = FactoryBot.create(:piece)
+    piece7 = FactoryBot.create(:piece)
+    piece8 = FactoryBot.create(:piece)
+    piece9 = FactoryBot.create(:piece)
+    piece10 = FactoryBot.create(:piece)
+    piece11 = FactoryBot.create(:piece)
+    piece12 = FactoryBot.create(:piece)
     FavoritePiece.create!(user_id: user.id, piece_id: piece1.id)
     FavoritePiece.create!(user_id: user.id, piece_id: piece3.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece4.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece5.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece6.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece7.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece8.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece9.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece10.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece11.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece12.id)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     # favorite_piece1 = FactoryBot.create(:favorite_piece, piece: piece1, user: user)
     # favorite_piece2 = FactoryBot.create(:favorite_piece, piece: piece3, user: user)
@@ -15,15 +33,17 @@ RSpec.describe "As an authenticated user" do
 
     visit(user_favorites_path)
 
+    expect(page).to have_css(".title", count: 11)
+
     within('.pieces') do
-      expect(page).to have_css('.titles')
-      expect(page).to have_css('.composers')
+      expect(page).to have_css('.title')
+      expect(page).to have_css('.composer')
       expect(page).to have_css('.contributions')
-      within(first('.titles')) do
-        expect(page).to have_content(piece1.title)
+      within(first('.title')) do
+        expect(page).to have_link(piece1.title)
       end
 
-      within(first('.composers')) do
+      within(first('.composer')) do
         expect(page).to have_content(piece1.composer)
       end
 
@@ -31,15 +51,9 @@ RSpec.describe "As an authenticated user" do
       #   expect(page).to have_content(piece1.contributions)
       # end
     end
+
   end
 end
-
-# Story:
-# As an authenticated user,
-# When I visit my favorites index page,
-# I should be able to see sections that include:
-#
-# Title of piece, which each title is a link to the piece’s show page
-# Composer of piece
+# TODO: Add pagination to this page so that we get up to 10 pieces on one page
 # Number of contributions to this piece
 # and I should also see a “Next Page” button at the bottom of the page

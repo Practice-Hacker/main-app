068a2f1ccf1f663c1046b07d654bf8416deb3c70
Eric Hale <eric_hale@comcast.net>
2020-09-14 18:25:44 -0600

    Add tests for favorite_pieces
    Co-authored-by: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>

--- spec/features/user/user_favorites_spec.rb
+++ spec/features/user/user_favorites_spec.rb
@@ -3,19 +3,33 @@ require 'rails_helper'
 RSpec.describe "As an authenticated user" do
   it "can show the title, and composer of my favorite peices on my favorites index page" do
     user = User.create!(username:"Tony Stark" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
+    piece1 = FactoryBot.create(:piece)
+    piece2 = FactoryBot.create(:piece)
+    piece3 = FactoryBot.create(:piece)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece1.id)
+    FavoritePiece.create!(user_id: user.id, piece_id: piece3.id)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    # favorite_piece1 = FactoryBot.create(:favorite_piece, piece: piece1, user: user)
+    # favorite_piece2 = FactoryBot.create(:favorite_piece, piece: piece3, user: user)
 
-    visit root_path
 
-    click_on('Log In')
+    visit(user_favorites_path)
 
-    click_on('Favorites')
-
-    expect(current_path).to eq(user_favorites_path)
-
-    within(first('.pieces')) do
+    within('.pieces') do
       expect(page).to have_css('.titles')
       expect(page).to have_css('.composers')
       expect(page).to have_css('.contributions')
+      within(first('.titles')) do
+        expect(page).to have_content(piece1.title)
+      end
+
+      within(first('.composers')) do
+        expect(page).to have_content(piece1.composer)
+      end
+
+      # within(first('.contributions')) do
+      #   expect(page).to have_content(piece1.contributions)
+      # end
     end
   end
 end

1a66564a65c18fa658d36f8701b2f7b133c9d3f9
twarbelow <taija.warbelow@google.com>
2020-09-14 22:13:53 -0700

    correct piece creation

--- spec/features/piece/show_spec.rb
+++ spec/features/piece/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "piece show page" do
   it "should have the name of the piece and composer" do
-    piece = Piece.create(title: "Such a good title", composer: "The bestest composer")
-    visit "/piece/#{piece.id}"
+    piece = Piece.create!(title: "Such a good title", subtitle: "NOPE", composer: "The bestest composer")
+    visit piece_show_path(piece.id)
     expect(page).to have_content(piece.title)
     expect(page).to have_content(piece.composer)
   end

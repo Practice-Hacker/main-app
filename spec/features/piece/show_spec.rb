76f3509993750aa332704fcf6fac304978522565
twarbelow <taija.warbelow@google.com>
2020-09-16 15:36:10 -0700

    test for api based piece show page

--- spec/features/piece/show_spec.rb
+++ spec/features/piece/show_spec.rb
@@ -9,25 +9,11 @@ RSpec.describe "piece show page" do
   end
 
   it "should have the name of the piece and composer (api request test)" do
-    data_response = stub_request(:get, "#{ENV['API_SINATRA_URL']}search?q=violin").to_return(status: 200, body: File.read('spec/data/search_data.json'))
+    data_response = stub_request(:get, "#{ENV['API_SINATRA_URL']}piece/28072").to_return(status: 200, body: File.read('spec/data/piece_data.json'))
     parsed_data = JSON.parse(data_response.response.body, symbolize_names: true)
-    expected_piece = parsed_data[:results].first
-
-    visit root_path
-
-    within 'navbar' do
-      fill_in :q, with: 'violin'
-      click_button 'Search'
-    end
-
-    find('.piece:first a').click
-    expect(current_path).to eq("/pieces/#{expected_piece[:work][:id]}")
-    # click the first piece
-    # which should check if that piece is already in our database
-    # for this test it should not be in our db
-    # and it should create a row in our Piece table (we probably need to add a column for openopus id)
-    # and display that info in the show page
+    piece_id = parsed_data[:work][:id]
+    visit piece_show_path(piece_id)
+    expect(page).to have_content(parsed_data[:work][:title])
+    expect(page).to have_content(parsed_data[:composer][:name])
   end
 end
-
-# make piece titles into links to show page from search index

ada71bfeb3f9bc225a981d0f2d69802633bb938c
twarbelow <taija.warbelow@google.com>
2020-09-15 22:50:38 -0700

    create piece facade

---
+++ app/facades/piece_facade.rb
@@ -0,0 +1,9 @@
+class PieceFacade
+  def initialize(api_work_id)
+    data = get_by_work_id(api_work_id)
+    PieceData.new(data)
+  end
+
+  def self.get_by_api_work_id(api_work_id)
+    ApiCallService.new.get_by_work_id(api_work_id)
+  end

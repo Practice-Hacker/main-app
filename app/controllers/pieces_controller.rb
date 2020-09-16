f8f1d39fc42d256b9f39933c57e745a507710cfc
twarbelow <taija.warbelow@google.com>
2020-09-16 15:36:34 -0700

    refactor show action to account for pieces not in our db

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,11 +1,9 @@
 class PiecesController < ApplicationController
   def show
-    require 'pry'; binding.pry
     @piece = Piece.find_by({api_work_id:  params[:id]})
     if @piece.nil?
-      
-    # this needs to check if the piece is in our database
-    # if it isn't it needs to get the info from our sinatra api?
-    # make that info into a piece object to send to the view
+      facade = PieceFacade.new(params[:id])
+      @piece = facade.to_piece
+    end
   end
 end

45e6c1195b0d1b28ff55e40c769a17f735343096
twarbelow <taija.warbelow@google.com>
2020-09-15 22:51:13 -0700

    get_by_work method, get_piece_ method

--- app/services/api_call_service.rb
+++ app/services/api_call_service.rb
@@ -3,7 +3,15 @@ class ApiCallService
     JSON.parse(get_search(query).body, symbolize_names: true)
   end
 
+  def get_by_work_id(api_work_id)
+    JSON.parse(get_piece(api_work_id).body, symbolize_names: true)
+  end
+
   private
+
+  def get_piece(api_work_id)
+    conn.get("piece/#{api_work_id}")
+  end
 
   def get_search(query)
     conn.get('search') do |f|

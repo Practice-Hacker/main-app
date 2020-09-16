435af671575652eca4f8fec882dd008f568f1c38
twarbelow <taija.warbelow@google.com>
2020-09-16 16:25:48 -0700

    add api_work_id to piece

--- spec/features/piece/show_spec.rb
+++ spec/features/piece/show_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "piece show page" do
   it "should have the name of the piece and composer (dummy data test)" do
-    piece = Piece.create!(title: "Such a good title", subtitle: "no subtitle here", composer: "The bestest composer")
-    visit piece_show_path(piece.id)
+    piece = Piece.create!(title: "Such a good title", subtitle: "no subtitle here", composer: "The bestest composer", api_work_id: "150")
+    visit piece_show_path(piece.api_work_id)
     expect(page).to have_content(piece.title)
     expect(page).to have_content(piece.composer)
   end

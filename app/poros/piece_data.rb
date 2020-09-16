88195ea0ced0d4272c38c5d69439dedb3d84d6f5
twarbelow <taija.warbelow@google.com>
2020-09-15 22:50:03 -0700

    update attribute names, add api_work_id

--- app/poros/piece_data.rb
+++ app/poros/piece_data.rb
@@ -1,8 +1,8 @@
 class PieceData
-  attr_reader :title, :name, :subtitle, :api_work_id
+  attr_reader :composer, :title, :subtitle, :api_work_id
   def initialize(data)
-    @name = data[:composer][:name]
-    @composer = data[:work][:title]
+    @composer = data[:composer][:name]
+    @title = data[:work][:title]
     @subtitle = data[:work][:subtitle]
     @api_work_id = data[:work][:id]
   end

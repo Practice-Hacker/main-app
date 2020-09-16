954ed90e1f73a9599e3fb3e69dd2e57c7cc90c46
twarbelow <taija.warbelow@google.com>
2020-09-15 22:49:29 -0700

    middle of work on piece controller

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,6 +1,9 @@
 class PiecesController < ApplicationController
   def show
-    @piece = Piece.find(params[:id])
+    require 'pry'; binding.pry
+    @piece = Piece.find_by({api_work_id:  params[:id]})
+    if @piece.nil?
+      
     # this needs to check if the piece is in our database
     # if it isn't it needs to get the info from our sinatra api?
     # make that info into a piece object to send to the view

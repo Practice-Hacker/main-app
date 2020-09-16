ef8f72664f66e8d10b5da30ac99b2855adca533b
twarbelow <taija.warbelow@google.com>
2020-09-16 15:37:30 -0700

    remove unnecessary init

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,5 +1,5 @@
 class Piece < ApplicationRecord
-  validates_presence_of :title, :subtitle, :composer
+  validates_presence_of :title, :subtitle, :composer, :api_work_id
 
   has_many :tips
   has_many :favorite_pieces

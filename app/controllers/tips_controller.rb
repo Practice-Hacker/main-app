38a9a7ac76a743109f9bb361a5c4406bc99e9dc6
Cam C <ckc.cameron@gmail.com>
2020-09-17 12:56:43 -0600

    Add destroy action to TipsController

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -24,6 +24,12 @@ class TipsController < ApplicationController
     redirect_to "/pieces/#{tip.piece_id}"
   end
 
+  def destroy
+    tip = Tip.find(params[:id])
+    tip.destroy
+    redirect_to "/pieces/#{tip.piece_id}"
+  end
+
   private
 
   def tip_params

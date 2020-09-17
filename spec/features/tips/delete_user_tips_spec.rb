21a86c4015e16c5cf1a7ca3a2c678287abab2867
Cam C <ckc.cameron@gmail.com>
2020-09-17 12:07:35 -0600

    Remove before :each block that gives spec errors

--- spec/features/tips/delete_user_tips_spec.rb
+++ spec/features/tips/delete_user_tips_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "user tips deletion" do
-  before :each do
-    @user = User.create!(username: "Bruce Banner" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
-    @piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
+  it "shows a link next to each tip to Delete Tip" do
+    user = User.create!(username: "Victor Shade" ,uid: "12345678" ,access_token: "token" ,email: "victor@shade.com")
+    piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
+
     visit root_path
     click_on "Log In"
-  end
 
-  it "shows a link next to each tip to Delete Tip" do
-    tip1 = Tip.create!(tip: "Something about classical music", difficulty_rating: 2, user: @user, piece: @piece)
-    tip2 = Tip.create!(tip: "Something else about classical music", difficulty_rating: 3, user: @user, piece: @piece)
-    tip3 = Tip.create!(tip: "Some more about classical music", difficulty_rating: 4, user: @user, piece: @piece)
+    tip1 = Tip.create!(tip: "Something about classical music", difficulty_rating: 2, user: user, piece: piece)
+    tip2 = Tip.create!(tip: "Something else about classical music", difficulty_rating: 3, user: user, piece: piece)
+    tip3 = Tip.create!(tip: "Some more about classical music", difficulty_rating: 4, user: user, piece: piece)
 
     tips = [tip1, tip2, tip3]
 
-    visit "/pieces/#{@piece.id}"
+    visit "/pieces/#{piece.id}"
 
     tips.each do |tip|
       within "#tips-#{tip.id}" do
@@ -25,19 +24,25 @@ RSpec.describe "user tips deletion" do
   end
 
   it "allows user to delete their tip from a piece" do
-    tip1 = Tip.create!(tip: "Something about classical music", difficulty_rating: 2, user: @user, piece: @piece)
-    tip2 = Tip.create!(tip: "Something else about classical music", difficulty_rating: 3, user: @user, piece: @piece)
-    tip3 = Tip.create!(tip: "Some more about classical music", difficulty_rating: 4, user: @user, piece: @piece)
+    user = User.create!(username: "Victor Shade" ,uid: "12345678" ,access_token: "token" ,email: "victor@shade.com")
+    piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
+
+    visit root_path
+    click_on "Log In"
+
+    tip1 = Tip.create!(tip: "Something about classical music", difficulty_rating: 2, user: user, piece: piece)
+    tip2 = Tip.create!(tip: "Something else about classical music", difficulty_rating: 3, user: user, piece: piece)
+    tip3 = Tip.create!(tip: "Some more about classical music", difficulty_rating: 4, user: user, piece: piece)
 
     tips = [tip1, tip2, tip3]
 
-    visit "/pieces/#{@piece.id}"
+    visit "/pieces/#{piece.id}"
 
     within "#tips-#{tip1.id}" do
       click_on "Delete Tip"
     end
 
-    expect(current_path).to eq("/pieces/#{@piece.id}")
+    expect(current_path).to eq("/pieces/#{piece.id}")
     expect(page).to_not have_css("#tips-#{tip1.id}")
     expect(tips).to eq([tip2, tip3])
   end

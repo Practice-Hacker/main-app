cd609a233ef6d7bc9904bc6e6994d09c2be8e083
twarbelow <taija.warbelow@gmail.com>
2020-09-16 22:01:41 -0700

    create action with flash message

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -1,4 +1,30 @@
 class UserFavoritesController < ApplicationController
+  def create
+    favorite = FavoritePiece.new
+    show_id = params[:api_work_id]
+    favorite.user_id = params[:user_id]
+    favorite.piece_id = params[:piece_id]
+    if favorite.save!
+      flash[:success] = "This piece has been added to your favorites."
+      redirect_to piece_show_path(show_id)
+    end
+  end
+
   def index
   end
+
+  def delete
+    flash[:success] = "This piece has been removed from your favorites."
+  end
 end
+#
+# def create
+#   friend = User.find_by(email: params[:email_id])
+#   if friend
+#     current_user.add_friend(friend)
+#     flash[:notice] = "#{friend.first_name} has been added as your friend"
+#   else
+#     flash[:alert] = 'There is no user with that email address'
+#   end
+#   redirect_to root_path
+# end

2391e2d6c79e96af20c71693cf5c0df01320757f
twarbelow <taija.warbelow@gmail.com>
2020-09-17 13:35:51 -0700

    comment in test necessities

---
+++ spec/features/favorites/index_page_sections_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "as a user on my favorites index page" do
+  it "each piece title should be a link, and each piece should have the composer of that piece and the total number of contributions to that piece" do
+    # need user
+    # need multiple pieces
+    # need different numbers of tips on each piece
+    # expect page to have these sections for each piece
+      # title as link to piece show page
+      # composer
+      # total number of users tips on that piece
+  end
+end

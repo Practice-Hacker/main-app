0225b71fd097069bb85d3e90a031b54e6c0405a9
twarbelow <taija.warbelow@gmail.com>
2020-09-16 23:08:01 -0700

    break spec into two tests

--- spec/features/user/toggle_favorites_spec.rb
+++ spec/features/user/toggle_favorites_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "as a user on a piece show page" do
-  it "I can favorite and unfavorite a piece from that piece's show page" do
+  it "I can favorite a piece from that piece's show page" do
     user = User.create!(username:"Tony Stark" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
     data_response = stub_request(:get, "#{ENV['API_SINATRA_URL']}piece/28072").to_return(status: 200, body: File.read('spec/data/piece_data.json'))
     parsed_data = JSON.parse(data_response.response.body, symbolize_names: true)
@@ -13,6 +13,19 @@ RSpec.describe "as a user on a piece show page" do
     click_link "Favorite"
     expect(current_path).to eq("/pieces/#{piece_id}")
     expect(page).to have_content("This piece has been added to your favorites.")
+  end
+
+  it "can unfavorite a piece from that piece's show page" do
+    user = User.create!(username:"Tony Stark" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
+    data_response = stub_request(:get, "#{ENV['API_SINATRA_URL']}piece/28072").to_return(status: 200, body: File.read('spec/data/piece_data.json'))
+    parsed_data = JSON.parse(data_response.response.body, symbolize_names: true)
+    piece_id = parsed_data[:work][:id]
+    piece = Piece.create!(title: "Such a good title", subtitle: "no subtitle here", composer: "The bestest composer", api_work_id: "#{piece_id}")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    user.favorite_pieces.create!(user_id: user.id, piece_id: piece.id)
+    user.save!
+
+    visit piece_show_path(piece_id)
     expect(page).to have_link("Remove Favorite")
     click_link "Remove Favorite"
     expect(current_path).to eq("/pieces/#{piece_id}")

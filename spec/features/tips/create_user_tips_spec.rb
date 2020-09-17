65717eb995a72ebb934dd8a7fb4628aac3a6909a
Cam C <ckc.cameron@gmail.com>
2020-09-17 13:03:22 -0600

    Fix username validation error in tip creation spec

--- spec/features/tips/create_user_tips_spec.rb
+++ spec/features/tips/create_user_tips_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "user tips creation" do
-  before :each do
-    @user = User.create!(username: "Bruce Banner" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
-    @piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
-    visit root_path
-    click_on "Log In"
-  end
-
   describe "when a user visits a piece show page" do
     it "displays a link for the user to add a tip to the piece" do
-      visit "/pieces/#{@piece.id}"
+      user = User.create!(username: "King T'Challa" ,uid: "12345678" ,access_token: "token" ,email: "tchalla@king.com")
+      piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
+      visit root_path
+      click_on "Log In"
+      visit "/pieces/#{piece.id}"
 
       expect(page).to have_link("Add Tip")
 
       click_on "Add Tip"
 
-      expect(current_path).to eq("/pieces/#{@piece.id}/tips/new")
+      expect(current_path).to eq("/pieces/#{piece.id}/tips/new")
     end
   end
 
   describe "when a user clicks the link to add a tip to a piece" do
     it "allows user to create a new tip" do
-      visit "/pieces/#{@piece.id}"
+      user = User.create!(username: "King T'Challa" ,uid: "12345678" ,access_token: "token" ,email: "tchalla@king.com")
+      piece = Piece.create!(title: "Do Re Mi", subtitle: "C Scale", composer: "Sir Wigwearer")
+      visit root_path
+      click_on "Log In"
+
+      visit "/pieces/#{piece.id}"
 
       click_on "Add Tip"
 
@@ -32,7 +34,7 @@ RSpec.describe "user tips creation" do
 
       user_tip = Tip.last
 
-      expect(current_path).to eq("/pieces/#{@piece.id}")
+      expect(current_path).to eq("/pieces/#{piece.id}")
       expect(user_tip.tip).to eq("Do the thing, yeah!")
       expect(user_tip.difficulty_rating).to eq(3)
 

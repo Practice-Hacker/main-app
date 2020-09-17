8edb90a4e37cbc7a62f4677b6ac56fa7896ed35b
twarbelow <taija.warbelow@gmail.com>
2020-09-16 22:22:09 -0700

    subtitle is optional

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Piece, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :composer }
+    it { should_not validate_presence_of :subtitle }
   end
-  
+
   describe 'relationships' do
     it {should have_many :favorite_pieces}
     it {should have_many :tips}

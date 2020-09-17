e8c76e5d041eff278f09e654e887cfb8147433b7
twarbelow <taija.warbelow@gmail.com>
2020-09-16 21:34:15 -0700

    remove comments and save and open page

--- spec/features/user/toggle_favorites_spec.rb
+++ spec/features/user/toggle_favorites_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "as a user on a piece show page" do
-  it "I can favorite and unfavorite a piece" do
+  it "I can favorite and unfavorite a piece from that piece's show page" do
     user = User.create!(username:"Tony Stark" ,uid: "12345678" ,access_token: "token" ,email: "tony@stark.com")
     data_response = stub_request(:get, "#{ENV['API_SINATRA_URL']}piece/28072").to_return(status: 200, body: File.read('spec/data/piece_data.json'))
     parsed_data = JSON.parse(data_response.response.body, symbolize_names: true)
@@ -10,7 +10,6 @@ RSpec.describe "as a user on a piece show page" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit piece_show_path(piece_id)
-    save_and_open_page
     click_link "Favorite"
     expect(current_path).to eq("piece_show_path(piece_id)")
     expect(page).to have_link("Remove Favorite")
@@ -19,11 +18,3 @@ RSpec.describe "as a user on a piece show page" do
     expect(page).to have_link("Favorite")
   end
 end
-
-# Story:
-# As an authenticated user,
-# When I visit a piece’s show page,
-# I should be able to toggle favorites on/off to add/remove them from my favorites,
-# And I should see a flash message that the piece was successfully favorited/unfavorited,
-# And depending on the status of the piece being a favorite already or not,
-# the button text should say “Favorite” (if not favorited already) or “Unfavorite” (if favorited already).

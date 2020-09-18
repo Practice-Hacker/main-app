e9270dd6faff55603a224edac29dfb7dea5426ce
Cam C <ckc.cameron@gmail.com>
2020-09-18 13:02:13 -0600

    Fix spec error for welcome page explanations

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,11 +5,19 @@ RSpec.describe 'Welcome page' do
     visit root_path
 
     title = 'PracticeHacker'
-    explanation = 'This is a classical music app'
+    explanation = 'Hack your practice routine with Practice Hacker!'
+    explanation2 = 'Search for the piece you are working on, and browse through user contributed tips for each piece.'
+    explanation3 = 'Our users are able to share their experience and knowledge by offering advice on mastering parts they found difficult.'
+    explanation4 = 'You can share anything from etudes to different fingerings to try.'
+    explanation5 = 'What worked for you may just work for someone else!'
     expect(page).to have_content(title)
     expect(page).to have_link('Login With Google')
     within '.welcome-main_content' do
       expect(page).to have_content(explanation)
+      expect(page).to have_content(explanation2)
+      expect(page).to have_content(explanation3)
+      expect(page).to have_content(explanation4)
+      expect(page).to have_content(explanation5)
     end
   end
 end

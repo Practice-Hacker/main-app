5046af4a5efedcf2589d99ab671a68fac8dd281e
twarbelow <taija.warbelow@gmail.com>
2020-09-17 22:40:11 -0700

    only call api once, add offset to arguments

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,13 +1,13 @@
 class SearchFacade
-  def self.pieces(query)
-    return [] if search_data(query)[:results].empty?
+  def self.pieces(query, offset)
+    res = search_data(query)[:results] || []
 
-    search_data(query)[:results].map do |result|
+    res.map do |result|
       PieceData.new(result)
     end
   end
 
-  def self.search_data(query)
-    ApiCallService.new.parsed_search(query)
+  def self.search_data(query, offset)
+    ApiCallService.new.parsed_search(query, offset)
   end
 end
